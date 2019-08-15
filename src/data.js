b8505abed72a0ac92da12d4f549c1345c8d9b7bd
Safe_Mode <safe2mode@gmail.com>
2019-08-15 23:26:45 +0300

    Create task data structure

---
+++ src/data.js
@@ -0,0 +1,33 @@
+export default {
+  description: [
+    `Изучить теорию`,
+    `Сделать домашку`,
+    `Пройти интенсив на соточку`,
+  ][Math.floor(Math.random() * 3)],
+  dueDate: Date.now() + [-1, 1][Math.floor(Math.random() * 2)] * Math.floor(Math.random() * 7) * 24 * 60 * 60 * 1000,
+  repeatingDays: {
+    mo: false,
+    tu: false,
+    we: Boolean(Math.round(Math.random())),
+    th: false,
+    fr: false,
+    sa: false,
+    su: false,
+  },
+  tags: [
+    `homework`,
+    `theory`,
+    `practice`,
+    `intensive`,
+    `keks`,
+  ].slice(0, Math.floor(Math.random() * 4)),
+  color: [
+    `black`,
+    `yellow`,
+    `blue`,
+    `green`,
+    `pink`,
+  ][Math.floor(Math.random() * 5)],
+  isFavorite: Boolean(Math.round(Math.random())),
+  isArchive: Boolean(Math.round(Math.random())),
+};
